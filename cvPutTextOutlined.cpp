00a24c18d8985003b868b44606c193bfb092319f
Juha Reunanen <juha.reunanen@tomaattinen.com>
2018-07-15 12:26:38 +0300

    First version

---
+++ cvPutTextOutlined.cpp
@@ -0,0 +1,14 @@
+﻿#include "cvPutTextOutlined.h"
+#include <opencv2/imgproc/imgproc.hpp>
+
+void cvPutTextOutlined(
+    cv::InputOutputArray img, const cv::String& text, cv::Point org,
+    int fontFace, double fontScale,
+    cv::Scalar colorInner, cv::Scalar colorOuter,
+    int thicknessInner, int additionalThicknessOuter,
+    int lineType, bool bottomLeftOrigin)
+{
+    const int thicknessOuter = thicknessInner + additionalThicknessOuter;
+    cv::putText(img, text, org, fontFace, fontScale, colorOuter, thicknessOuter, lineType, bottomLeftOrigin);
+    cv::putText(img, text, org, fontFace, fontScale, colorInner, thicknessInner, lineType, bottomLeftOrigin);
+}
---
+++ cvPutTextOutlined.h
@@ -0,0 +1,8 @@
+﻿#include <opencv2/core/core.hpp>
+
+void cvPutTextOutlined(
+    cv::InputOutputArray img, const cv::String& text, cv::Point org,
+    int fontFace = cv::FONT_HERSHEY_SIMPLEX, double fontScale = 0.75,
+    cv::Scalar colorInner = cvScalarAll(255), cv::Scalar colorOuter = cvScalarAll(0),
+    int thicknessInner = 1, int additionalThicknessOuter = 1,
+    int lineType = cv::LINE_8, bool bottomLeftOrigin = false);
